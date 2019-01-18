0974afc9e7503d19afba0e48854445bc344de34b
gsienkiewicz <gsienkiewicz@macadamian.com>
2019-01-18 14:33:46 -0500

    homepage displays all current categories along with the latest added items

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-from app import app
+from app import app, db
+from app.models import Category, Item
 
 from flask import render_template, flash, redirect
 from flask_login import login_required, logout_user
@@ -7,7 +8,17 @@ from flask_login import login_required, logout_user
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    categories = Category.query.all()
+    # Limit the items query limit to contain no more
+    # latest items then there are categories
+    items_query_limit = 10 if len(categories) < 10 else len(categories)
+    items = Item.query.order_by(Item.date.desc()).limit(10).all()
+    return render_template(
+        "index.html",
+        title="Catalog App",
+        categories=categories,
+        items=items
+    )
 
 
 @app.route("/logout")
