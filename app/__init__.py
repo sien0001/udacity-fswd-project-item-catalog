a3c3637f2b63459c746258dcbfde00b0021be248
Greg Sienkiewicz <sien0001@gmail.com>
2019-01-17 19:54:58 -0500

    Add the index route and basic Flask with Bootstrap extension config

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,14 @@
 from config import Config, GoogleAuthConfig
+
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+# Initialize the Flask app, load in the Config via object
+# and attach the Bootstrap extension
+app = Flask(__name__)
+app.config.from_object(Config)
+Bootstrap(app)
+
+
+# Configure the routes
+from app import views
