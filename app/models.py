1b53b1c11904250b35506b6aeffa845242b7052c
gsienkiewicz <gsienkiewicz@macadamian.com>
2019-01-18 13:13:23 -0500

    Added the Category and Item models

--- app/models.py
+++ app/models.py
@@ -19,3 +19,27 @@ class OAuth(db.Model, OAuthConsumerMixin):
     user_id = db.Column(db.Integer, db.ForeignKey(User.id))
 
     user = db.relationship(User)
+
+
+class Category(db.Model):
+    __tablename__ = "categories"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, index=True)
+
+    items = db.relationship("Item", backref="item", lazy=True)
+
+
+class Item(db.Model):
+    __tablename__ = "items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    category_id = db.Column(
+        db.Integer,
+        db.ForeignKey(Category.id),
+        nullable=False
+    )
+    title = db.Column(db.String(100), unique=True, index=True)
+    description = db.Column(db.String(250))
+
+    category = db.relationship(Category)
